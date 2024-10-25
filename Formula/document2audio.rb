e775772f08e1b306f2aec114580cf01d91492ce6
Shyamal Chandra <sapanamicrosoftware@gmail.com>
2024-10-25 12:22:59 -0500

    Initial commit with document2audio formula

--- Formula/document2audio.rb
+++ Formula/document2audio.rb
@@ -1,7 +1,7 @@
 class Document2audio < Formula
     desc "Convert PDF documents to audio and play on VLC with M3U playlist"
-    homepage "https://github.com/Sapana-Micro-Software/document2audio"
-    url "https://github.com/Sapana-Micro-Software/document2audio/archive/refs/tags/v0.0.1.tar.gz"
+    homepage "https://github.com/Sapana-Micro-Software/Document2Audio"
+    url "https://github.com/Sapana-Micro-Software/Document2Audio/archive/refs/tags/v0.0.1.tar.gz"
     sha256 "aab4a71e7680b8691118ef6835208b3cc825f99de54680caca45521f6ff2f7ed"
     license "Copyright (c) 2024 Sapana Micro Software"
   
