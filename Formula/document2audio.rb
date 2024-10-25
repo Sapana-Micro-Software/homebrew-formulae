c6ae073d93eac4f015512494605394e387b9fff5
Shyamal Chandra <sapanamicrosoftware@gmail.com>
2024-10-25 13:12:02 -0500

    New sha256

--- Formula/document2audio.rb
+++ Formula/document2audio.rb
@@ -12,6 +12,7 @@ class Document2audio < Formula
     def install
       bin.install "document2audio.sh" => "document2audio"
       chmod 0755, bin/"document2audio"
+      system "chmod", "+x", bin/"document2audio"
     end
   
     test do
