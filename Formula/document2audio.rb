3c9f9c61a99f89c9ee7eef013c9c4716a17e9038
Shyamal Chandra <sapanamicrosoftware@gmail.com>
2024-10-25 12:47:30 -0500

    New sha256

--- Formula/document2audio.rb
+++ Formula/document2audio.rb
@@ -2,7 +2,7 @@ class Document2audio < Formula
     desc "Convert PDF documents to audio and play on VLC with M3U playlist"
     homepage "https://github.com/Sapana-Micro-Software/Document2Audio"
     url "https://github.com/Sapana-Micro-Software/Document2Audio/archive/refs/tags/v0.0.1.tar.gz"
-    sha256 "aab4a71e7680b8691118ef6835208b3cc825f99de54680caca45521f6ff2f7ed"
+    sha256 "d13016f67bdda487b589e0dca17bcd2594f3b2f2a116b88db8fba52f66c0548e"
     license "Copyright (c) 2024 Sapana Micro Software"
   
     depends_on "imagemagick"
